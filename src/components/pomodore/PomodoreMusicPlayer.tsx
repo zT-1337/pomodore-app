b424e64fa68d11631a0c8b08f75c2edb64983c1e
Thomas Zerr <thomas.zerr@gmail.com>
2021-07-11 14:39:58 +0200

    -fixed: videos playing even though prop is false by showing the videos but moving them out of the viewport and adding overflow hidden to the body (i am a g)

--- src/components/pomodore/PomodoreMusicPlayer.tsx
+++ src/components/pomodore/PomodoreMusicPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { CSSProperties, useState } from "react"
 import { PomodoreMusicPlayerState } from "../../interfaces/states/PomodoreMusicPlayerState"
 import ReactPlayer from "react-player";
 import { PomodoreMusicPlayerProps } from "../../interfaces/props/pomodore/PomodoreMusicPlayerProps";
@@ -11,6 +11,11 @@ const initialState = (): PomodoreMusicPlayerState => {
 
 export function PomodoreMusicPlayer(props: PomodoreMusicPlayerProps) {
   const [state, setState] = useState(initialState());
+
+  const videoStyle: CSSProperties = {
+    position: "absolute",
+    top: "1000px"
+  }
   
   const onVolumeChange = (event: React.FormEvent<HTMLInputElement>) => {
     setState({
@@ -25,8 +30,8 @@ export function PomodoreMusicPlayer(props: PomodoreMusicPlayerProps) {
   return (
     <div className="PomodoreMusicPlayer">
       <input type="range" min="0" max="1" step={0.01} value={state.volume} onChange={onVolumeChange}></input>
-      <ReactPlayer url={props.workMusicUrl} width={267} height={150} playing={playingWorkMusic} loop={true} volume={state.volume}></ReactPlayer>
-      <ReactPlayer url={props.pauseMusicUrl} width={267} height={150} playing={playingPauseMusic} loop={true} volume={state.volume}></ReactPlayer>
+      <ReactPlayer url={props.workMusicUrl} style={videoStyle} playing={playingWorkMusic} loop={true} volume={state.volume}></ReactPlayer>
+      <ReactPlayer url={props.pauseMusicUrl} style={videoStyle} playing={playingPauseMusic} loop={true} volume={state.volume}></ReactPlayer>
     </div>
   )
 }
